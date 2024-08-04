8d9cf05b8c77b76bd7725d2b7eeeab7e7f90138d
Christopher Morton <sonro@gmx.com>
2024-08-04 14:46:29 +0100

    test: math impl set value

--- src/bit_board/MathImpl.zig
+++ src/bit_board/MathImpl.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const testing = std.testing;
 const bit_board = @import("../bit_board.zig");
 const MaskInt = bit_board.MaskInt;
 const ShiftInt = bit_board.ShiftInt;
@@ -22,8 +23,16 @@ pub fn set(mask: *MaskInt, index: u8) void {
     mask.* |= maskBit(index);
 }
 
+pub fn setRange(mask: *MaskInt, range: Range) void {
+    mask.* |= createRangeMask(range, true);
+}
+
 pub fn unset(mask: *MaskInt, index: u8) void {
     mask.* &= ~maskBit(index);
+}
+
+pub fn unsetRange(mask: *MaskInt, range: Range) void {
+    mask.* &= ~createRangeMask(range, true);
 }
 
 pub fn toggle(mask: *MaskInt, index: u8) void {
@@ -43,4 +52,42 @@ fn createRangeMask(range: Range, value: bool) MaskInt {
         mask &= std.math.boolMask(MaskInt, value) >> shift;
     }
     return mask;
+}
+
+const full_mask: MaskInt = ~@as(MaskInt, 0);
+const empty_mask: MaskInt = 0;
+
+test "set value lowest true empty mask" {
+    try test_set_value_empty_mask(0, true, 1);
+}
+
+test "set value lowest true full mask" {
+    try test_set_value_full_mask(0, true, full_mask);
+}
+
+test "set value lowest false empty mask" {
+    try test_set_value_empty_mask(0, false, 0);
+}
+
+test "set value lowest false full mask" {
+    const expected = full_mask << 1;
+    try test_set_value_full_mask(0, false, expected);
+}
+
+test "set value highest true empty mask" {
+    const index = size - 1;
+    const expected = @as(MaskInt, 1) << index;
+    try test_set_value_empty_mask(index, true, expected);
+}
+
+fn test_set_value_empty_mask(index: u8, value: bool, expected: MaskInt) !void {
+    var mask = empty_mask;
+    setValue(&mask, index, value);
+    try testing.expectEqual(expected, mask);
+}
+
+fn test_set_value_full_mask(index: u8, value: bool, expected: MaskInt) !void {
+    var mask = full_mask;
+    setValue(&mask, index, value);
+    try testing.expectEqual(expected, mask);
 }
