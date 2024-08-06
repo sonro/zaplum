800faa1aa8db1cac3c3746c7ba66bdb5a75345b2
Christopher Morton <sonro@gmx.com>
2024-08-06 14:28:58 +0100

    test: bitboard fully

--- src/chess/BitBoard.zig
+++ src/chess/BitBoard.zig
@@ -153,7 +153,7 @@ pub fn isSubsetOf(self: BitBoard, other: BitBoard) bool {
 /// Returns true if the first board is the superset
 /// of the second one.
 pub fn isSupersetOf(self: BitBoard, other: BitBoard) bool {
-    return other.subsetOf(self);
+    return other.isSubsetOf(self);
 }
 
 /// Returns the complement boards. Squares in the result
@@ -267,4 +267,454 @@ fn initEmpty() BitBoard {
 
 fn initFull() BitBoard {
     return .{ .mask = std.math.maxInt(MaskInt) };
+}
+
+test "empty" {
+    const expected = BitBoard{ .mask = 0 };
+    try testing.expectEqual(expected, empty);
+    try testing.expectEqual(expected.mask, empty.mask);
+}
+
+test "full" {
+    const expected = BitBoard{ .mask = std.math.maxInt(MaskInt) };
+    try testing.expectEqual(expected, full);
+    try testing.expectEqual(expected.mask, full.mask);
+}
+
+test "from" {
+    const expected = BitBoard{ .mask = 0x0f0f };
+    try testing.expectEqual(expected, from(0x0f0f));
+}
+
+test "is set" {
+    const board = from(0b1001);
+    try testing.expect(board.isSet(0));
+    try testing.expect(!board.isSet(1));
+    try testing.expect(!board.isSet(2));
+    try testing.expect(board.isSet(3));
+}
+
+test "pop count empty" {
+    try testing.expectEqual(@as(IndexInt, 0), empty.count());
+}
+
+test "pop count full" {
+    try testing.expectEqual(@as(IndexInt, size), full.count());
+}
+
+test "pop count custom" {
+    const board = from(0b11111111_00000001);
+    try testing.expectEqual(@as(IndexInt, 9), board.count());
+}
+
+test "set value is set true" {
+    var board = empty;
+    board.setValue(0, true);
+    try testing.expect(board.isSet(0));
+}
+
+test "set value is set false" {
+    var board = full;
+    board.setValue(4, false);
+    try testing.expect(!board.isSet(4));
+}
+
+test "set is set empty" {
+    var board = empty;
+    board.set(12);
+    try testing.expect(board.isSet(12));
+}
+
+test "set is set full" {
+    var board = full;
+    board.set(14);
+    try testing.expect(board.isSet(14));
+}
+
+test "unset is not set full" {
+    var board = full;
+    board.unset(16);
+    try testing.expect(!board.isSet(16));
+}
+
+test "unset is not set empty" {
+    var board = empty;
+    board.unset(17);
+    try testing.expect(!board.isSet(17));
+}
+
+test "set range value are set true" {
+    var board = empty;
+    board.setRangeValue(.{ .start = 0, .end = 3 }, true);
+    for (0..3) |i| {
+        try testing.expect(board.isSet(@intCast(i)));
+    }
+}
+
+test "set range value are set false" {
+    var board = full;
+    board.setRangeValue(.{ .start = 0, .end = 3 }, false);
+    for (0..3) |i| {
+        try testing.expect(!board.isSet(@intCast(i)));
+    }
+}
+
+test "toggle once" {
+    var board = from(0b1010);
+    board.toggle(0);
+    try testing.expectEqual(from(0b1011), board);
+}
+
+test "toggle twice" {
+    var board = from(0b1010);
+    board.toggle(0);
+    board.toggle(0);
+    try testing.expectEqual(from(0b1010), board);
+}
+
+test "toggle all empty to full" {
+    var board = empty;
+    board.toggleAll();
+    try testing.expectEqual(full, board);
+}
+
+test "toggle all full to empty" {
+    var board = full;
+    board.toggleAll();
+    try testing.expectEqual(empty, board);
+}
+
+test "toggle all custom" {
+    var board = from(0b1010);
+    board.toggleAll();
+    var expected = full;
+    expected.mask ^= 0b1010;
+    try testing.expectEqual(expected, board);
+}
+
+test "toggle set empty with full" {
+    var board = empty;
+    board.toggleSet(full);
+    try testing.expectEqual(full, board);
+}
+
+test "toggle set full with full" {
+    var board = full;
+    board.toggleSet(full);
+    try testing.expectEqual(empty, board);
+}
+
+test "toggle set full with empty" {
+    var board = full;
+    board.toggleSet(empty);
+    try testing.expectEqual(full, board);
+}
+
+test "toggle set empty with custom" {
+    var board = empty;
+    board.toggleSet(from(0b1010));
+    try testing.expectEqual(from(0b1010), board);
+}
+
+test "toggle set custom with custom" {
+    var board = from(0b1010);
+    board.toggleSet(from(0b0101));
+    try testing.expectEqual(from(0b1111), board);
+}
+
+test "set union empty with empty" {
+    var board = empty;
+    board.setUnion(empty);
+    try testing.expectEqual(empty, board);
+}
+
+test "set union full with full" {
+    var board = full;
+    board.setUnion(full);
+    try testing.expectEqual(full, board);
+}
+
+test "set union empty with full" {
+    var board = empty;
+    board.setUnion(full);
+    try testing.expectEqual(full, board);
+}
+
+test "set union custom with full" {
+    var board = from(0b1010);
+    board.setUnion(full);
+    try testing.expectEqual(full, board);
+}
+
+test "set union custom with custom" {
+    var board = from(0b1010);
+    board.setUnion(from(0b0101));
+    try testing.expectEqual(from(0b1111), board);
+}
+
+test "set intersection empty with empty" {
+    var board = empty;
+    board.setIntersection(empty);
+    try testing.expectEqual(empty, board);
+}
+
+test "set intersection full with full" {
+    var board = full;
+    board.setIntersection(full);
+    try testing.expectEqual(full, board);
+}
+
+test "set intersection empty with full" {
+    var board = empty;
+    board.setIntersection(full);
+    try testing.expectEqual(empty, board);
+}
+
+test "set intersection custom with full" {
+    var board = from(0b1010);
+    board.setIntersection(full);
+    try testing.expectEqual(from(0b1010), board);
+}
+
+test "set intersection custom with custom" {
+    var board = from(0b1110);
+    board.setIntersection(from(0b0101));
+    try testing.expectEqual(from(0b0100), board);
+}
+
+test "find first set empty" {
+    var board = empty;
+    try testing.expectEqual(null, board.findFirstSet());
+}
+
+test "find first set full" {
+    var board = full;
+    try testing.expectEqual(0, board.findFirstSet());
+}
+
+test "find first set custom" {
+    var board = from(0b1010);
+    try testing.expectEqual(1, board.findFirstSet());
+}
+
+test "pop bit empty" {
+    var board = empty;
+    try testing.expectEqual(null, board.popBit());
+}
+
+test "pop bit full" {
+    var board = full;
+    try testing.expectEqual(0, board.popBit());
+    try testing.expectEqual(1, board.popBit());
+}
+
+test "pop bit custom" {
+    var board = from(0b1010);
+    try testing.expectEqual(1, board.popBit());
+    try testing.expectEqual(3, board.popBit());
+    try testing.expectEqual(null, board.popBit());
+}
+
+test "eql" {
+    var board = from(0b1010);
+    try testing.expect(board.eql(from(0b1010)));
+    try testing.expect(!board.eql(from(0b0101)));
+}
+
+test "is subset of empty empty" {
+    var board = empty;
+    try testing.expect(board.isSubsetOf(empty));
+}
+
+test "is subset of empty full" {
+    var board = empty;
+    try testing.expect(board.isSubsetOf(full));
+}
+
+test "is subset of full empty" {
+    var board = full;
+    try testing.expect(!board.isSubsetOf(empty));
+}
+
+test "is subset of custom" {
+    var board = from(0b1010);
+    try testing.expect(board.isSubsetOf(from(0b11111010)));
+    try testing.expect(!board.isSubsetOf(from(0b1000)));
+}
+
+test "is superset of empty empty" {
+    var board = empty;
+    try testing.expect(board.isSupersetOf(empty));
+}
+
+test "is superset of empty full" {
+    var board = empty;
+    try testing.expect(!board.isSupersetOf(full));
+}
+
+test "is superset of full empty" {
+    var board = full;
+    try testing.expect(board.isSupersetOf(empty));
+}
+
+test "is superset of custom" {
+    var board = from(0b1010);
+    try testing.expect(!board.isSupersetOf(from(0b1110)));
+    try testing.expect(board.isSupersetOf(from(0b1000)));
+}
+
+test "complement empty" {
+    const board = empty;
+    const actual = board.complement();
+    try testing.expectEqual(full, actual);
+}
+
+test "complement full" {
+    const board = full;
+    const actual = board.complement();
+    try testing.expectEqual(empty, actual);
+}
+
+test "complement custom" {
+    const board = from(0b1010);
+    const expected = from(~board.mask);
+    const actual = board.complement();
+    try testing.expectEqual(expected, actual);
+}
+
+test "union empty with empty" {
+    const board = empty;
+    const actual = board.unionWith(empty);
+    try testing.expectEqual(empty, actual);
+}
+
+test "union empty with full" {
+    const board = empty;
+    const actual = board.unionWith(full);
+    try testing.expectEqual(full, actual);
+}
+
+test "union full with full" {
+    const board = full;
+    const actual = board.unionWith(full);
+    try testing.expectEqual(full, actual);
+}
+
+test "union custom with custom" {
+    const board = from(0b1010);
+    const actual = board.unionWith(from(0b0101));
+    try testing.expectEqual(from(0b1111), actual);
+}
+
+test "intersect empty with empty" {
+    const board = empty;
+    const actual = board.intersectWith(empty);
+    try testing.expectEqual(empty, actual);
+}
+
+test "intersect empty with full" {
+    const board = empty;
+    const actual = board.intersectWith(full);
+    try testing.expectEqual(empty, actual);
+}
+
+test "intersect full with full" {
+    const board = full;
+    const actual = board.intersectWith(full);
+    try testing.expectEqual(full, actual);
+}
+
+test "intersect custom to 0" {
+    const board = from(0b1010);
+    const actual = board.intersectWith(from(0b0101));
+    try testing.expectEqual(from(0b0000), actual);
+}
+
+test "intersect custom to 1" {
+    const board = from(0b0001);
+    const actual = board.intersectWith(from(0b1111));
+    try testing.expectEqual(from(0b0001), actual);
+}
+
+test "xor empty with empty" {
+    const board = empty;
+    const actual = board.xorWith(empty);
+    try testing.expectEqual(empty, actual);
+}
+
+test "xor empty with full" {
+    const board = empty;
+    const actual = board.xorWith(full);
+    try testing.expectEqual(full, actual);
+}
+
+test "xor full with full" {
+    const board = full;
+    const actual = board.xorWith(full);
+    try testing.expectEqual(empty, actual);
+}
+
+test "xor custom with custom" {
+    const board = from(0b1010);
+    const actual = board.xorWith(from(0b1101));
+    try testing.expectEqual(from(0b0111), actual);
+}
+
+test "difference with empty" {
+    const board = from(0b1010);
+    const actual = board.differenceWith(empty);
+    try testing.expectEqual(from(0b1010), actual);
+}
+
+test "difference with full" {
+    const board = from(0b1010);
+    const actual = board.differenceWith(full);
+    try testing.expectEqual(from(0b0000), actual);
+}
+
+test "difference with custom" {
+    const board = from(0b1010);
+    const actual = board.differenceWith(from(0b0111));
+    try testing.expectEqual(from(0b1000), actual);
+}
+
+test "iterate empty" {
+    const board = empty;
+    var iter = board.iterator(.{});
+    var x: usize = 0;
+    while (iter.next()) |_| : (x += 1) {}
+    try testing.expectEqual(0, x);
+}
+
+test "iterate full" {
+    const board = full;
+    var iter = board.iterator(.{});
+    var x: usize = 0;
+    while (iter.next()) |_| : (x += 1) {}
+    try testing.expectEqual(size, x);
+}
+
+test "iterate custom" {
+    const board = from(0b1010);
+    var iter = board.iterator(.{});
+    try testing.expectEqual(1, iter.next());
+    try testing.expectEqual(3, iter.next());
+    try testing.expectEqual(null, iter.next());
+}
+
+test "iterate custom reverse" {
+    const board = from(0b1010);
+    var iter = board.iterator(.{ .direction = .reverse });
+    try testing.expectEqual(3, iter.next());
+    try testing.expectEqual(1, iter.next());
+    try testing.expectEqual(null, iter.next());
+}
+
+test "iterate custom unset" {
+    const board = from(0b1010);
+    var iter = board.iterator(.{ .kind = .unset });
+    try testing.expectEqual(0, iter.next());
+    try testing.expectEqual(2, iter.next());
+    try testing.expectEqual(4, iter.next());
+    try testing.expectEqual(5, iter.next());
 }
