6ea2b20512b2c2ca8a03add0e75f3e4b3a40b84d
Christopher Morton <sonro@gmx.com>
2024-08-13 08:14:39 +0100

    test: bit piece map extra

--- src/chess/bit_piece_map/Extra.zig
+++ src/chess/bit_piece_map/Extra.zig
@@ -42,3 +42,30 @@ pub fn update(self: *Extra, map: *const BitPieceMap, piece: Piece) void {
     self.color[piece.side().toU2()].setUnion(board);
     self.kind[piece.kind().toU3()].setUnion(board);
 }
+
+test "init" {
+    var map = BitPieceMap.empty;
+    map.setSquare(.white_pawn, .a2);
+    map.setSquare(.white_knight, .b1);
+    map.setSquare(.black_knight, .b8);
+    map.setSquare(.black_queen, .d8);
+
+    const extra = Extra.init(&map);
+    try testing.expectEqual(extra.all, map.getAll());
+    try testing.expectEqual(extra.color[Color.white.toU1()], map.getColor(.white));
+    try testing.expectEqual(extra.color[Color.black.toU1()], map.getColor(.black));
+    try testing.expectEqual(extra.kind[Piece.Kind.pawn.toU3()], map.getKind(.pawn));
+    try testing.expectEqual(extra.kind[Piece.Kind.knight.toU3()], map.getKind(.knight));
+    try testing.expectEqual(extra.kind[Piece.Kind.queen.toU3()], map.getKind(.queen));
+}
+
+test "update" {
+    var map = BitPieceMap.empty;
+    var extra = Extra.init(&map);
+    map.setSquare(.white_pawn, .a2);
+
+    extra.update(&map, .white_pawn);
+    try testing.expectEqual(extra.all, map.getAll());
+    try testing.expectEqual(extra.color[Color.white.toU1()], map.getColor(.white));
+    try testing.expectEqual(extra.kind[Piece.Kind.pawn.toU3()], map.getKind(.pawn));
+}
