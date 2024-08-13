a3c521e5efc998a1bd74a670f70b6de32d434ebe
Christopher Morton <sonro@gmx.com>
2024-08-13 15:03:12 +0100

    feat: position hashing non destructive methods

--- src/chess/PosHash.zig
+++ src/chess/PosHash.zig
@@ -5,7 +5,6 @@ const testing = std.testing;
 const assert = std.debug.assert;
 
 const chess = @import("../chess.zig");
-const Color = chess.Color;
 const Piece = chess.Piece;
 const Square = chess.Square;
 
@@ -28,25 +27,47 @@ pub fn init() PosHash {
 }
 
 pub fn setPieceSquare(self: *PosHash, piece: Piece, square: Square) void {
-    assert(piece != .none);
-    assert(square != .none);
-    self.key ^= hash_keys.piece_map[piece.toU4()][square.toIndex()];
+    self.* = self.withPieceSquare(piece, square);
 }
 
 pub fn setSide(self: *PosHash) void {
-    self.key ^= hash_keys.side;
+    self.* = self.withSide();
 }
 
 pub fn setCastleMap(self: *PosHash, castle_state: anytype) void {
+    self.* = self.withCastleMap(castle_state);
+}
+
+pub fn setEnPassantSquare(self: *PosHash, ep_square: Square) void {
+    self.* = self.withEnPassantSquare(ep_square);
+}
+
+pub fn withPieceSquare(self: PosHash, piece: Piece, square: Square) PosHash {
+    assert(piece != .none);
+    assert(square != .none);
+    const key = self.key ^ hash_keys.piece_map[piece.toU4()][square.toIndex()];
+    return .{ .key = key };
+}
+
+pub fn withSide(self: PosHash) PosHash {
+    const key = self.key ^ hash_keys.side;
+    return .{ .key = key };
+}
+
+pub fn withCastleMap(self: PosHash, castle_state: anytype) PosHash {
     const T = @TypeOf(castle_state);
     if (T != chess.CastleState and T != chess.CastleStatePacked and T != u4) {
         @compileError("Expected `chess.CastleState` or `chess.CastleStatePacked` or `u4`");
     }
-    self.key ^= hash_keys.castle_map[castle_state.toU4()];
+    const val = if (T == u4) castle_state else castle_state.toU4();
+    const key = self.key ^ hash_keys.castle_map[val];
+    return .{ .key = key };
 }
 
-pub fn setEnPassantSquare(self: *PosHash, ep_square: Square) void {
-    self.key ^= hash_keys.en_passant[ep_square.toIndex()];
+pub fn withEnPassantSquare(self: PosHash, ep_square: Square) PosHash {
+    assert(ep_square != .none);
+    const key = self.key ^ hash_keys.en_passant[ep_square.toIndex()];
+    return .{ .key = key };
 }
 
 pub fn eql(self: PosHash, other: PosHash) bool {
@@ -131,5 +152,38 @@ test "set piece square" {
     actual.setPieceSquare(.white_pawn, .a1);
     try testing.expect(!original.eql(actual));
     actual.setPieceSquare(.white_pawn, .a1);
+    try testing.expectEqual(original, actual);
+}
+
+test "with piece square" {
+    const original = PosHash.init();
+    var actual = original.withPieceSquare(.white_pawn, .a1);
+    try testing.expect(!original.eql(actual));
+    actual = actual.withPieceSquare(.white_pawn, .a1);
+    try testing.expectEqual(original, actual);
+}
+
+test "with side" {
+    const original = PosHash.init();
+    var actual = original.withSide();
+    try testing.expect(!original.eql(actual));
+    actual = actual.withSide();
+    try testing.expectEqual(original, actual);
+}
+
+test "with castle map" {
+    const original = PosHash.init();
+    const castle_state = chess.CastleState.none;
+    var actual = original.withCastleMap(castle_state);
+    try testing.expect(!original.eql(actual));
+    actual = actual.withCastleMap(castle_state);
+    try testing.expectEqual(original, actual);
+}
+
+test "with en passant" {
+    const original = PosHash.init();
+    var actual = original.withEnPassantSquare(.c1);
+    try testing.expect(!original.eql(actual));
+    actual = actual.withEnPassantSquare(.c1);
     try testing.expectEqual(original, actual);
 }
